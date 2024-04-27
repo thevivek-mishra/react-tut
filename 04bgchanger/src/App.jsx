4f93f47258f47d06821f0b5a31ed843117aa148f
vivek kumar mishra <vivekmishra4701@gmail.com>
2024-04-27 12:19:01 +0530

    background changer

---
+++ 04bgchanger/src/App.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+
+export default function App() {
+  const [color ,setColor] =useState("olive")
+  return (
+ <>
+<div className="w-full h-screen duration-200"
+style={{backgroundColor:color}}>
+  <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+  <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+    <button className='outline-none px-4 py-2 rounded-full' style={{backgroundColor:"red"}} onClick={()=>setColor("red")}>Red</button>
+    <button className='outline-none px-4 py-2 rounded-full' style={{backgroundColor:"blue"}} onClick={()=>setColor("blue")}>blue</button>
+    <button className='outline-none px-4 py-2 rounded-full' style={{backgroundColor:"yellow"}} onClick={()=>setColor("yellow")}>yellow</button>
+    <button className='outline-none px-4 py-2 rounded-full' style={{backgroundColor:"green"}} onClick={()=>setColor("green")}>green</button>
+  </div>
+</div>
+</div>
+
+
+
+ </>
+  )
+}
